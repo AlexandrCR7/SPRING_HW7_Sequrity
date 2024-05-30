ee612e292966417e642827011e99611af0c3da2b
Alexandr <k9108@mail.ru>
2024-05-30 12:22:27 +0300

    Add new HW

---
+++ SpringBoot3.2.0/src/main/java/ru/gb/Sem7_HW7/service/HomeService.java
@@ -0,0 +1,12 @@
+package ru.gb.Sem7_HW7.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+	
+	public String getText() {
+		return "Home";
+	}
+
+}
---
+++ SpringBoot3.2.0/src/main/java/ru/gb/Sem7_HW7/service/UserService.java
@@ -0,0 +1,11 @@
+package ru.gb.Sem7_HW7.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	public String getText() {
+		return "User";
+	}
+}
